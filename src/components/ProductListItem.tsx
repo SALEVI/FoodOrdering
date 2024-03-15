9aa2fbcf669739a440b08d66ab25ec0a9ad02521
salevi <aleventesalcudean@gmail.com>
2024-03-15 17:25:00 +0200

    Added Prettier workspace config

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,9 +1,9 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
-import Colors from '../constants/Colors';
-import { Product } from '../types';
+import { StyleSheet, Text, View, Image } from "react-native";
+import Colors from "../constants/Colors";
+import { Product } from "../types";
 
 export const defaultPizzaImage =
-  'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
 
 type ProductListItemProps = {
   product: Product;
@@ -13,7 +13,7 @@ const ProductListItem = ({ product }: ProductListItemProps) => {
     <View style={styles.container}>
       <Image
         source={{ uri: product.image || defaultPizzaImage }}
-         style={styles.image}
+        style={styles.image}
         resizeMode="contain"
       />
       <Text style={styles.title}>{product.name}</Text>
@@ -26,23 +26,23 @@ export default ProductListItem;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     padding: 10,
     borderRadius: 20,
     flex: 1,
-    maxWidth: '50%',
+    maxWidth: "50%",
   },
   image: {
-    width: '100%',
+    width: "100%",
     aspectRatio: 1,
   },
   title: {
     fontSize: 18,
-    fontWeight: '600',
+    fontWeight: "600",
     marginVertical: 10,
   },
   price: {
     color: Colors.light.tint,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });
